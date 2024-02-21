7cce9715a6bd3f6d4589368f343acc7f6cafca5b
kong <skjava79@gmail.com>
2024-02-21 18:58:45 +0700

    update w1

--- tmp/kong/W1/flask/manage.py
+++ tmp/kong/W1/flask/manage.py
@@ -13,18 +13,5 @@ def create_db():
     db.create_all()
     db.session.commit()
 
-@cli.command("seed_db")
-def seed_db():
-    db.session.add(AuthUser(email="flask@204212", name='สมชาย ทรงแบด',
-        password=generate_password_hash('1234',
-        method='sha256'),
-        avatar_url='https: /ui-avatars.com/api/?name=\
-        สมชาย+ทรงแบด&background=83ee03&color=fff'))
-    
-    db.session.add(
-        PrivateContact(firstname='ส้มโอ', lastname='โอเค',
-                        phone='081-111-1112', owner_id=1))
-    db.session.commit()
-
 if __name__ == "__main__":
     cli()
