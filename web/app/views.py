f92c31882a04b49af09cc1cf97a34c90595892d3
Chaipat J. <work.chaipat@gmail.com>
2024-03-08 05:16:00 +0700

    fix flash not work on login page

--- web/app/views.py
+++ web/app/views.py
@@ -403,6 +403,7 @@ def pre3_login():
             # check if the user actually exists
             # take the user-supplied password, hash it, and compare it to the
             # hashed password in the database
+
             if not user or not check_password_hash(user.password, password):
                 flash("Please check your login details and try again.")
                 # if the user doesn't exist or password is wrong, reload the page
